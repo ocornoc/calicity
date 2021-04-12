6bebaa62765b9d5ef3e2f7f70e406ff9dcc29ee6
ocornoc <ocornoc@protonmail.com>
2021-04-11 17:31:51 -0700

    add lots of docs, some bugfixes

--- calicity-knowledge/src/belief.rs
+++ calicity-knowledge/src/belief.rs
@@ -66,17 +66,25 @@ impl SavingsCurve {
         a2: Nf32::unchecked_new( -1.00E-06),
     } };
 
+    /// The "savings", or in our use case, memorability of something, given
+    /// seconds from last ideal recall `t`.
     pub fn savings(&self, t: Nf32) -> f32 {
         let rhs = self.mu2 * (self.a1 * t).exp();
         (self.a1 * t).exp().mul_add(*self.mu1, *rhs)
     }
 
+    /// Update the constants to decay slower, modelling how repeated recalling
+    /// and repeated.
     pub fn recall(&mut self) {
         self.a1 *= 0.6666666666;
         self.a2 *= 0.6666666666;
     }
 }
 
+/// A belief "facet", or something an entity could have beliefs about.
+///
+/// Given an example "I believe your eyes are blue", the facet is "your eye
+/// color" and the [belief value](BeliefValue) is "blue".
 pub trait BeliefFacet: Debug + Hash + Eq + Clone {
     type Value: BeliefValue<Facet = Self>;
 }
@@ -127,34 +135,43 @@ pub enum EvidenceKind {
     Eavesdropping { speaker: ThingIdx, spoken_to: ThingIdx },
 }
 
+/// Data regarding the strength or weight assigned to a piece of
+/// [evidence](OccasionalEvidence).
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct StrengthData {
+    /// The strength as of last update.
     pub strength: Strength,
-    pub peak_strength: Strength,
+    /// The scale applied to the output of the curve.
+    pub scale: Strength,
+    /// The curve used to deteriorate the strength of the evidence.
     pub curve: SavingsCurve,
+    /// The time offset in seconds, or how old the piece of evidence is.
     pub offset: Nf32,
 }
 
 impl StrengthData {
     pub const DEFAULT: Self = unsafe { StrengthData {
         strength: NotNan::unchecked_new(1000.0),
-        peak_strength: NotNan::unchecked_new(1000.0),
+        scale: NotNan::unchecked_new(1000.0),
         curve: SavingsCurve::DROS,
         offset: NF32_ZERO,
     } };
 
+    /// Deteriorate the strength.
     pub fn deteriorate(&mut self, dt: RelativeTime) {
         self.offset += dt.num_minutes() as f32;
         self.update_strength();
     }
 
+    /// Update the strength to be the calculated at this time.
     pub fn update_strength(&mut self) {
-        self.strength = self.peak_strength * self.curve.savings(self.offset);
+        self.strength = self.scale * self.curve.savings(self.offset);
     }
 
+    /// Reset the strength to be zero and update the strength.
     pub fn reset(&mut self) {
-        self.strength = self.peak_strength;
         self.offset = NF32_ZERO;
+        self.update_strength();
     }
 }
 
@@ -164,13 +181,28 @@ impl Default for StrengthData {
     }
 }
 
+/// Data regarding just how memorable a piece of [evidence](OccasionalEvidence)
+/// is.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct MemorabilityData {
+    /// How memorable the [evidence](OccasionalEvidence) is.
+    ///
+    /// This is used for calcuting whether to forget some evidence, and can be
+    /// used naturally to calculate whether to mutate some evidence.
     pub memorability: Memorability,
+    /// The curve used for updating the memorability.
     pub curve: SavingsCurve,
+    /// The number of seconds since this evidence was last successfully
+    /// remembered.
     pub seconds_since_last_recall: Nf32,
-    pub last_remember_attempt: Nf32,
-    pub memorability_at_last_remember: Memorability,
+    /// `seconds_since_last_recall` when the evidence was last attempted to be
+    /// forgotten.
+    pub last_forget_attempt: Nf32,
+    /// The memorability last time this evidence was attempted to be forgotten.
+    pub mem_at_forget_attempt: Memorability,
+    /// The curve prior to any recalls.
+    ///
+    /// Used for resetting the evidence.
     pub original_curve: SavingsCurve,
 }
 
@@ -179,46 +211,56 @@ impl MemorabilityData {
         memorability: NotNan::unchecked_new(1.0),
         curve: SavingsCurve::DROS,
         seconds_since_last_recall: NF32_ZERO,
-        last_remember_attempt: NF32_ZERO,
-        memorability_at_last_remember: NotNan::unchecked_new(1.0),
+        last_forget_attempt: NF32_ZERO,
+        mem_at_forget_attempt: NotNan::unchecked_new(1.0),
         original_curve: SavingsCurve::DROS,
     } };
 
+    /// Deteriorate the memorability.
     pub fn deteriorate(&mut self, dt: RelativeTime) {
         let seconds = dt.num_seconds() as f32;
         self.seconds_since_last_recall += seconds;
         self.update_memorability();
-        self.update_last_memorability();
     }
 
+    /// Update the memorability with the current curve.
     pub fn update_memorability(&mut self) {
         self.memorability = Nf32::new(self.curve.savings(self.seconds_since_last_recall)).unwrap();
     }
 
-    pub fn update_last_memorability(&mut self) {
-        self.memorability_at_last_remember = Nf32::new(
-            self.curve.savings(self.last_remember_attempt),
+    /// Update the memorability at the last attempted forget.
+    pub fn update_mem_at_forget(&mut self) {
+        self.mem_at_forget_attempt = Nf32::new(
+            self.curve.savings(self.last_forget_attempt),
         ).unwrap();
     }
 
+    /// Reset the memorability data, including the curve.
     pub fn reset(&mut self) {
         self.curve = self.original_curve;
         self.seconds_since_last_recall = NF32_ZERO;
-        self.last_remember_attempt = NF32_ZERO;
+        self.mem_at_forget_attempt = NF32_ZERO;
+        self.update_memorability();
+        self.update_mem_at_forget();
     }
 
+    /// Recall the data, resetting some fields and updating memorability.
     pub fn recall(&mut self) {
         self.seconds_since_last_recall = NF32_ZERO;
-        self.last_remember_attempt = NF32_ZERO;
-        self.memorability = unsafe { NotNan::unchecked_new(1.0) };
+        self.mem_at_forget_attempt = NF32_ZERO;
         self.curve.recall();
+        self.update_memorability();
+        self.update_mem_at_forget();
     }
 
+    /// Returns whether the evidence has been forgotten since the last time this
+    /// was called, assuming it was still remembered when last called.
     pub fn should_forget(&mut self, rng: &mut (impl Rng + ?Sized)) -> bool {
         let have_forgotten = rng.gen_bool(
-            (*self.memorability_at_last_remember - *self.memorability) as f64,
+            (*self.mem_at_forget_attempt - *self.memorability) as f64,
         );
-        self.last_remember_attempt = self.seconds_since_last_recall;
+        self.last_forget_attempt = self.seconds_since_last_recall;
+        self.update_mem_at_forget();
         have_forgotten
     }
 }
@@ -229,23 +271,39 @@ impl Default for MemorabilityData {
     }
 }
 
+/// A piece of evidence that isn't observational or reflexive.
 #[derive(Debug, Clone)]
 pub struct OccasionalEvidence {
+    /// The kind of evidence.
     pub kind: EvidenceKind,
+    /// The historical action associated with the evidence.
     pub action: PastActionIdx,
+    /// The strength of the evidence.
     pub strength: StrengthData,
+    /// The memorability data of the evidence.
     pub memorability: MemorabilityData,
 }
 
+/// All the data regarding the value of a [belief value](BeliefValue).
 #[derive(Debug, Clone, Default)]
 pub struct ValueData {
+    /// All of the associated [occasional evidence](OccasionalEvidence).
     pub occasional_evidence: Vec<OccasionalEvidence>,
+    /// Represents whether this value is a belief about the holder of the
+    /// belief.
+    ///
+    /// For example, "I know my eyes are blue."
     pub reflective: bool,
+    /// A "score" added to the strength to represent the sum of observational
+    /// evidence.
     pub observation_score: ObsScore,
+    /// Just how strongly this [belief value](BeliefValue) is believed.
     pub strength: Strength,
 }
 
 impl ValueData {
+    /// Deteriorate the [strength](StrengthData) and
+    /// [memorability](MemorabilityData).
     pub fn deteriorate(&mut self, dt: RelativeTime) {
         for evidence in &mut self.occasional_evidence {
             evidence.strength.deteriorate(dt);
@@ -272,6 +330,7 @@ impl ValueData {
         }
     }
 
+    /// Merge evidence, reflexivity, and observation score.
     pub fn merge(&mut self, vdata: ValueData) {
         self.reflective |= vdata.reflective;
         self.observation_score += vdata.observation_score;
@@ -279,16 +338,20 @@ impl ValueData {
         self.update_strength();
     }
 
+    /// Forget a piece of [occasional evidence](OccasionalEvidence).
     pub fn forget(&mut self, evidence: usize) {
         self.occasional_evidence.swap_remove(evidence);
     }
 
+    /// *Potentially* forget a piece of [evidence](OccasionalEvidence) based on
+    /// its [memorability](MemorabilityData).
     pub fn maybe_forget(&mut self, evidence: usize, rng: &mut (impl Rng + ?Sized)) {
         if self.occasional_evidence[evidence].memorability.should_forget(rng) {
             self.forget(evidence);
         }
     }
 
+    /// Try forgetting all [evidence](OccasionalEvidence).
     pub fn maybe_forget_all(&mut self, rng: &mut (impl Rng + ?Sized)) {
         for evidence in (0..self.occasional_evidence.len()).into_iter().rev() {
             self.maybe_forget(evidence, rng);
@@ -296,6 +359,9 @@ impl ValueData {
     }
 }
 
+/// A set of [beliefs](BeliefValue).
+///
+/// A map between [belief values](BeliefValue) and their [stats](ValueData).
 #[derive(Debug)]
 pub struct BeliefSet<V: BeliefValue>(pub HashMap<V, ValueData>);
 
